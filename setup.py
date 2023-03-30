8b862bf888b47a7e7aa88fc61f5cc76d51534bcc
Jonny-E <jonahsimon06@gmail.com>
2023-03-30 14:02:31 +0200

    updated setup.py

--- setup.py
+++ setup.py
@@ -1,19 +1,25 @@
-from setuptools import setup
+from setuptools import find_packages, setup
 import subprocess
 
 from sitesniffer import __version__
 
 subprocess.run("pip install -r requirements.txt", shell=True, check=True)
 
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+
+
 setup(
     name='sitesniffer',
     version=__version__,
     description='This is a Python script that can extract various information about a website, including its IP address, SSL certificate information, domain information, page load time, and other useful insights.',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url='https://github.com/thisisjsimon/SiteSniffer',
     author='Jonah Simon',
     author_email='thisisjsimon.github@gmail.com',
-    url='https://github.com/thisisjsimon/SiteSniffer',
-
-
+    license='MIT',
     py_modules=['sitesniffer'],
     install_requires=[
         "whois",
@@ -22,4 +28,7 @@ setup(
         "colorama",
         "requests"
     ],
+    extras_require={
+        "dev": ["pytest>=7.0", "twine>=4.0.2"]
+    }
 )
--- sitesniffer.py
+++ sitesniffer.py
@@ -230,5 +230,3 @@ def get_site_info(url):
 def print_dictionary(d):
     for key, value in d.items():
         print(f"\033[32m{key}\033[0m: \033[37m{value}\033[0m")
-
-print_dictionary(get_site_info("https://youtube.com"))
