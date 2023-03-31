3adb88a34168834f53c244f2452b6e0c1fd0b060
shouzy <shouzy@tuta.io>
2023-03-31 20:27:41 +0200

    removed colorama

--- setup.py
+++ setup.py
@@ -5,8 +5,8 @@ from setuptools import setup
 
 from src.sitesniffer import __author__, __license__, __title__, __version__
 
-with open("README.md", "r", encoding="utf-8") as fh:
-    long_description: str = fh.read()
+with open("README.md", "r", encoding="utf-8") as f:
+    long_description: str = f.read()
 
 setup(
     name=__title__,
@@ -19,6 +19,6 @@ setup(
     author_email="thisisjsimon.github@gmail.com",
     license=__license__,
     py_modules=[r"src/sitesniffer/"],
-    install_requires=["whois", "bs4", "idna", "colorama", "requests"],
-    extras_require={"dev": ["pytest>=7.0", "twine>=4.0.2"]},
+    install_requires=["whois", "bs4", "idna", "requests"],
+    extras_require={"dev": ["pytest", "twine"]},
 )
